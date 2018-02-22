a347eae51d438ad9697ed199faeab76f5f8bb02c
MikeTiff <mtiffany@mail.csuchico.edu>
2018-02-21 19:28:51 -0800

    So

---
+++ Lab.cpp
@@ -0,0 +1,35 @@
+
+//Written By: Michael Tiffany
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{  
+  string list[5]; //array of 5 strings
+  int numItems = 0;
+  char input;
+  do
+ {
+  cout<<"\n==GROCERY LIST MANAGER==";
+  cout<<"\nEnter your choice: ";
+  cout<<"\n (A)dd an item";
+  cout<<"\n (Q)uit";
+  cout<<"\nYour choice (A/Q): "<<endl;
+  cin>>input;
+  if(input == 'a' || input == 'A')
+  {
+  cout<<"What is the item?\n";
+  string item;
+  cin>>item;
+  list[numItems] = item;
+  numItems++;
+  
+   if(numItems = 5)
+  { cout<<"You'll need a bigger list!\n";
+  }}
+ }
+    while(input != 'q' && input != 'Q');
+    {}
+  return 0;
+}
