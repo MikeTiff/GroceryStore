492bca37e8c5a5f5d62be8a20d93badd469f31d6
MikeTiff <mtiffany@mail.csuchico.edu>
2018-02-21 19:01:42 -0800

    Maybee

--- main.cpp
+++ main.cpp
@@ -15,12 +15,12 @@ int main()
   cout<<"\nEnter your choice: ";
   cout<<"\n (A)dd an item";
   cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): \n";
+  cout<<"\nYour choice (A/Q): "<<endl;
   cin>>input;
   if(input == 'a' || input == 'A')
   {
   cout<<"What is the item?\n";
-  string item;
+  string item = " ";
   cin>>item;
   list[numItems] = item;
   numItems++;
