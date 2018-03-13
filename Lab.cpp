d7c0813c028ff1dd16bc429109ccb5763a15d5c5
MikeTiff <mtiffany@mail.csuchico.edu>
2018-03-12 17:43:21 -0700

    Week 8 Lab post TurnIn

--- Lab.cpp
+++ Lab.cpp
@@ -12,35 +12,35 @@ int main()
   int x;     
   char input;
   do
- {
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
-  if (input == 'a' || input == 'A')
    {
-   cout<<"What is the item?\n";
-   string item;
-   cin>>item;
-   list.push_back(item);
-   }
- }
- while (input != 'q' && input != 'Q');
-  {
-    if (list.empty() == true)
-    {
-     cout<<"No items to buy!\n";
-    }
-    else
-    {
-    cout<<"==ITEMS TO BUY==\n";
-    for (x=0; x < list.size(); x++)
+    cout<<"\n==GROCERY LIST MANAGER==";
+    cout<<"\nEnter your choice: ";
+    cout<<"\n (A)dd an item";
+    cout<<"\n (Q)uit";
+    cout<<"\nYour choice (A/Q): ";
+    cin>>input;
+    if (input == 'a' || input == 'A')
      {
-      cout<<list[x]<<endl;
+      cout<<"What is the item?\n";
+      string item;
+      cin>>item;
+      list.push_back(item);
      }
-    }
-  }
+   }
+  while (input != 'q' && input != 'Q');
+   {
+    if (list.empty() == true)
+     {
+      cout<<"No items to buy!\n";
+     }
+    else
+     {
+      cout<<"==ITEMS TO BUY==\n";
+      for (x=0; x < list.size(); x++)
+       {
+        cout<< x+1 <<" "<<list[x]<<endl;
+       }
+     }
+   }
   return 0;
 }
